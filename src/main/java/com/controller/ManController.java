4d17c7469ed2a943ea738f04dc6d84f9b0ff6960
yura <yurabh1990@gmail.com>
2021-03-06 14:51:55 +0200

    Add swagger functionality to the controllers classes.

--- src/main/java/com/controller/ManController.java
+++ src/main/java/com/controller/ManController.java
@@ -2,62 +2,93 @@ package com.controller;
 
 import com.domain.Man;
 import com.service.ManService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("man")
+@Api(value = "ManController")
 public class ManController {
 
+    @ApiModelProperty(notes = "ManService",
+            name = "manService",
+            required = true, value = "service")
     private final ManService manService;
 
     @Autowired
-    public ManController(ManService manService) {
+    public ManController(final ManService manService) {
         this.manService = manService;
     }
 
+    @ApiOperation(value = "Save Man in the System",
+            response = Man.class,
+            tags = "saveMan")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success OK"),
+            @ApiResponse(code = 401, message = "not authorized"),
+            @ApiResponse(code = 403, message = "forbidden"),
+            @ApiResponse(code = 404, message = "not found")})
     @PostMapping("/save")
     public Man saveAndReturnMan(@RequestBody final Man man) {
         return manService.save(man);
     }
 
-
+    @ApiOperation(value = "Get Man in the System",
+            response = Man.class,
+            tags = "getManById")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success OK"),
+            @ApiResponse(code = 401, message = "not authorized"),
+            @ApiResponse(code = 403, message = "forbidden"),
+            @ApiResponse(code = 404, message = "not found")})
     @GetMapping(value = "/get/{manId}")
     public ResponseEntity<Man> getManById(@PathVariable("manId") final int manId) {
         return new ResponseEntity<>(manService.find(manId), HttpStatus.OK);
     }
 
-
+    @ApiIgnore
     @PutMapping(value = "/update")
     public void update(@RequestBody final Man man) {
         manService.update(man);
     }
 
+    @ApiIgnore
     @DeleteMapping("/delete/{manId}")
     public void delete(@PathVariable final int manId) {
         manService.delete(manId);
     }
 
+    @ApiOperation(value = "GetAll Men in the System", tags = "getAllMen")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success OK"),
+            @ApiResponse(code = 401, message = "not authorized"),
+            @ApiResponse(code = 403, message = "forbidden"),
+            @ApiResponse(code = 404, message = "not found")})
     @GetMapping(value = "/get/all/men")
     public List<Man> getAll() {
         return manService.getAll();
     }
 
+    @ApiIgnore
     @GetMapping(value = "/findByNameAndAge")
     public Man findByNameAndAge(@RequestParam("name") final String name
             , @RequestParam("age") final int age) {
         return manService.findByNameAndAge(name, age);
     }
 
-    @GetMapping(value = "/findAllName")
-    public List<Man> findByNameAndAge(@RequestParam("name") final String name) {
+    @ApiIgnore
+    @GetMapping(value = "/findAllByName")
+    public List<Man> findAllByName(@RequestParam("name") final String name) {
         return manService.findAllByName(name);
     }
 
+    @ApiIgnore
     @GetMapping(value = "/findByNameAndAgeSort")
     public List<Man> findByNameAndAgeSort(@RequestParam("name") final String name) {
         return manService.findByNameAndAgeAndSortByName(name);
--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -2,6 +2,11 @@ package com.controller;
 
 import com.domain.User;
 import com.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +14,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
+@Api(value = "UserController")
 public class UserController {
 
+    @ApiModelProperty(notes = "User Service",
+            name = "userService",
+            required = true, value = "service")
     private final UserService userService;
 
     @Autowired
@@ -18,21 +27,51 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiOperation(value = "Save User in the System",
+            response = User.class,
+            tags = "saveUser")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized"),
+            @ApiResponse(code = 403, message = "forbidden"),
+            @ApiResponse(code = 404, message = "not found")})
     @PostMapping("/save")
     public User saveAndReturnMan(@RequestBody final User user) {
         return userService.save(user);
     }
 
+    @ApiOperation(value = "Get User from the System",
+            response = User.class,
+            tags = "getUserById")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized"),
+            @ApiResponse(code = 403, message = "forbidden"),
+            @ApiResponse(code = 404, message = "not found")})
     @GetMapping(value = "/get/{id}")
     public ResponseEntity<User> getManById(@PathVariable("id") final int userId) {
         return new ResponseEntity<>(userService.find(userId), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Update User from the System",
+            tags = "updateUser")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized"),
+            @ApiResponse(code = 403, message = "forbidden"),
+            @ApiResponse(code = 404, message = "not found")})
     @PutMapping(value = "/update")
     public void update(@RequestBody final User user) {
         userService.update(user);
     }
 
+    @ApiOperation(value = "Delete User from the System",
+            tags = "deleteUser")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized"),
+            @ApiResponse(code = 403, message = "forbidden"),
+            @ApiResponse(code = 404, message = "not found")})
     @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable("id") final int userId) {
         userService.delete(userId);
